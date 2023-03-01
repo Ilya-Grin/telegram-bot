a500171f71fb855ee2fbf604cea47a89357337a4
Ilya_Grin <47398274+Ilya-Grin@users.noreply.github.com>
2023-03-01 15:39:21 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,65 +1,63 @@
-import os
-import requests
+import asyncio
 import logging
-from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+import os
 
-# Load environment variables from .env file
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Command
+from aiogram.types import ParseMode
+from binance.client import AsyncClient, BinanceAPIException
+from dotenv import load_dotenv
+
 load_dotenv()
 
-# Set logging level to WARNING
-logging.basicConfig(level=logging.WARNING)
+# Load Telegram bot token from environment variable
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 
-# Initialize Telegram bot and dispatcher
-bot = Bot(token=os.environ['TELEGRAM_API_TOKEN'])
-dp = Dispatcher(bot)
+bot = Bot(token=TELEGRAM_BOT_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
-# Binance endpoint for getting the price of a specific cryptocurrency
-binance_api_endpoint = 'https://api.binance.com/api/v3/ticker/price'
+# Set up logger
+logging.basicConfig(filename="bot.log", level=logging.ERROR)
 
-# Define a function to get the price of a cryptocurrency from Binance
-
-
-def get_crypto_price(symbol):
-    try:
-        params = {'symbol': symbol}
-        headers = {'X-MBX-APIKEY': os.environ['BINANCE_API_KEY']}
-        response = requests.get(binance_api_endpoint,
-                                params=params, headers=headers)
-        response.raise_for_status()
-        price = response.json()['price']
-        return price
-    except requests.exceptions.HTTPError as e:
-        logging.warning(
-            f'Binance API вернул {e.response.status_code} {e.response.reason}: {e.response.text}')
-        return None
-    except Exception as e:
-        logging.warning(f'Ошибка получения цены для {symbol}: {str(e)}')
-        return None
-
-# Define a handler for the /start command
+# Create asynchronous Binance API client
+BINANCE_API_KEY = os.environ.get("BINANCE_API_KEY")
+BINANCE_API_SECRET = os.environ.get("BINANCE_API_SECRET")
+client = AsyncClient(BINANCE_API_KEY, BINANCE_API_SECRET)
 
 
 @dp.message_handler(commands=['start'])
-async def start_command(message: types.Message):
-    await message.reply("Привет! Я бот для проверки цены криптовалют на Binance. Отправь мне команду /price и код валюты (например, /price BTCUSDT), чтобы получить её текущую цену. 😊")
-
-# Define a handler for the /price command
+async def cmd_start(message: types.Message):
+    await message.reply("Hi there! I'm a bot that can retrieve the current price of cryptocurrencies from Binance. To get started, type /price [symbol], where [symbol] is the ticker symbol of the cryptocurrency you want to get the price for.")
 
 
-@dp.message_handler(commands=['price'])
-async def price_command(message: types.Message):
+@dp.message_handler(Command("price"))
+async def cmd_price(message: types.Message):
+    symbol = message.get_args().upper()
+    if not symbol:
+        await message.reply("Please enter a cryptocurrency symbol after the command. For example: /price BTCUSDT")
+        return
     try:
-        symbol = message.text.split()[1].upper()
-        price = get_crypto_price(symbol)
-        if price is not None:
-            await message.reply(f'Текущая цена {symbol} - {price} USDT. 🚀')
+        ticker = await client.get_symbol_ticker(symbol=symbol)
+        price = ticker['price']
+        await message.reply(f"{symbol} current price is {price}")
+    except BinanceAPIException as e:
+        if e.code == -1121:
+            await message.reply(f"Invalid symbol {symbol}. Please enter a valid symbol.")
+        elif e.code == -2010:
+            await message.reply(f"{symbol} withdrawals are disabled.")
         else:
-            await message.reply('Ошибка получения цены, попробуйте позже. 😕')
-    except:
-        await message.reply('Пожалуйста, введите корректный код валюты после команды /price. 😕')
+            await message.reply(f"An error occurred while retrieving {symbol} price. {e}")
+    except Exception as e:
+        logging.error(e)
+        await message.reply("An error occurred. Please try again later.")
 
-# Start the bot
+
+async def main():
+    await dp.start_polling()
+
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
